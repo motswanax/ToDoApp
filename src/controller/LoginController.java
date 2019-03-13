8932875dd4b3cafe2a57eca708a7997c3ce95563
Baiketli Dijo <baiketlid@gmail.com>
2019-03-13 21:15:55 +0000

    - added event handling to the loginSignupButton to make it open the signup view when clicked - fixed the link to the controller for login.fxml

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -4,7 +4,12 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -32,9 +37,43 @@ public class LoginController {
 
     @FXML
     void initialize() {
-        loginButton.setOnAction(actionEvent -> {
-            System.out.println("Login clicked");
+        String loginText = loginUsername.getText().trim();
+        String loginPwd = loginPassword.getText().trim();
+
+        System.out.println("adfd" + loginText);
+
+        loginSignUpButton.setOnAction(actionEvent1 -> {
+            System.out.println("haha");
+            // take user to sign up screen
+            loginSignUpButton.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/view/signup.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setTitle("Sign Up");
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
         });
 
+        loginButton.setOnAction(actionEvent -> {
+            if (!loginText.equals("") || !loginPwd.equals("")) {
+                loginUser(loginText, loginPwd);
+            } else {
+                System.out.println("Error login in user");
+            }
+        });
+
+    }
+
+    private void loginUser(String username, String password) {
+        // check if user exists in the database. If true,
+        // take them to AddItem view
     }
 }
