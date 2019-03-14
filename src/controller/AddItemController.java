7ebb9f0077e749b3e5698e2f707a18a00ac63d7c
Baiketli Dijo <baiketlid@gmail.com>
2019-03-14 16:26:06 +0000

    - added the addItemForm.fxml view which will replace the addButton on addItem view when it is clicked. - added the AddItemFormController for the view - modified the AddItemController to move the label and button out of the way when the button is clicked, and added animations to the movement.

--- src/controller/AddItemController.java
+++ src/controller/AddItemController.java
@@ -1,9 +1,12 @@
 package controller;
 
 import animations.Shaker;
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -22,11 +25,39 @@ public class AddItemController {
     private ImageView addButton;
 
     @FXML
+    private Label noTaskLabel;
+
+    @FXML
     void initialize() {
         addButton.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
             Shaker buttonShaker = new Shaker(addButton);
             buttonShaker.shake();
+
+            // add transition animation to the button and label
+            FadeTransition fadeTransition = new FadeTransition(Duration.millis(2000), addButton);
+            FadeTransition labelTransition = new FadeTransition(Duration.millis(2000), noTaskLabel);
+
+            // remove
             System.out.println("Added Clicked");
+            addButton.relocate(0, 20);
+            noTaskLabel.relocate(0, 85);
+
+            addButton.setOpacity(0);
+            noTaskLabel.setOpacity(0);
+
+            // transition the button
+            fadeTransition.setFromValue(1f);
+            fadeTransition.setToValue(0f);
+            fadeTransition.setCycleCount(1);
+            fadeTransition.setAutoReverse(false);
+            fadeTransition.play();
+
+            // transition the label
+            labelTransition.setFromValue(1f);
+            labelTransition.setToValue(0);
+            labelTransition.setCycleCount(1);
+            labelTransition.setAutoReverse(false);
+            labelTransition.play();
         });
     }
 }
